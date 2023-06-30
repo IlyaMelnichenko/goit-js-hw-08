6eb07e8b21a5a35cc1708acf1014b1525aae9c23
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-07-01 00:20:50 +0300

    dkkg

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+
+
 // Change code below this line
 
-console.log(galleryItems);
+
+const refs = {
+    list : document.querySelector('.gallery'),
+
+};
+const listItemsGalery = creatItemsGalery(galleryItems);
+refs.list.insertAdjacentHTML("beforeend", listItemsGalery);
+document.addEventListener('DOMContentLoaded', onItemClick);
+function onItemClick(event){
+    event.preventDefault();
+    var lightbox = new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+
+};
+
+// створення розмітки
+
+
+function creatItemsGalery(items){
+   return items.map(item=>
+    `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" alt="${item.description}">
+    </a> </li>`).join(``);
+};
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+
+const player = new Player('vimeo-player', {
+    src: "https://player.vimeo.com/video/236203659",
+    width: 640
+});
+
+player.on('timeupdate', function(data){
+    localStorage.setItem("videoplayer-current-time",JSON.stringify(data.seconds));
+    console.log(data)
+});
+const startTime = JSON.parse(localStorage.getItem("videoplayer-current-time")) ?? 0;
+console.log(startTime);
+player.setCurrentTime(startTime);
 
