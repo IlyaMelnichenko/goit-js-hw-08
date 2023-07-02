6a01f3fc3768d4b1e2e1f02d4093b89ac4bbb512
IlyaMelnichenko <124725419+IlyaMelnichenko@users.noreply.github.com>
2023-07-02 20:28:43 +0000

    Deploying to gh-pages from @ IlyaMelnichenko/goit-js-hw-08@b8668f2d0e6f8db8d458448b468c7ea6e6e3df99 🚀

--- 03-feedback.4ab22c1c.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form"),a=e.querySelector('input[name="email"]'),t=e.querySelector('textarea[name="message"]'),n=(a.value,t.value,{});e.addEventListener("input",(function(e,a){n.email=e,n.message=a,localStorage.setItem("feedback-form-state",JSON.stringify(n))})),console.log(a.value)}();
-//# sourceMappingURL=03-feedback.4ab22c1c.js.map
